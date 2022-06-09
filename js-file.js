bbf0d80a9a8646cf94417ef8f202a777b2f4c664
Tyler Pardee <tjpardee@gmail.com>
2022-06-09 18:42:19 -0400

    Create Grid & Adjust Grid Button Functionality

---
+++ js-file.js
@@ -0,0 +1,54 @@
+/* Event Listeners */
+
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    for (let i = 0; i < 256; i++) {
+      const div = document.createElement("div");
+      div.className = "div-square";
+      container.appendChild(div);
+    }
+  },
+  false
+);
+
+const button = document.querySelector(".prompt");
+button.addEventListener("click", retrieveAnswer);
+
+const container = document.querySelector(".grid-container");
+
+/* Retrieve & Check Answer */
+
+function retrieveAnswer() {
+  let answer = prompt("Enter number of squares per side:");
+  let gridNumber = checkAnswer(answer);
+  adjustGrid(gridNumber);
+}
+
+function checkAnswer(Answer) {
+  let output = parseInt(Answer);
+
+  if (output > 100) {
+    alert("Please enter number between 1 and 100!");
+    retrieveAnswer();
+  } else {
+    return output;
+  }
+}
+
+/* Adjust Grid */
+
+function adjustGrid(gridNumber) {
+  const gridContainer = document.querySelector(".grid-container");
+
+  gridContainer.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+
+  let iterations = gridNumber * gridNumber;
+
+  console.log(iterations);
+  for (let i = 0; i < iterations; i++) {
+    const div = document.createElement("div");
+    div.className = "div-square";
+    container.appendChild(div);
+  }
+}
